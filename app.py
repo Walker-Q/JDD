ef67466e298f6411c87c6ce036b034286e589d4b
F兽 <54022108+aqz236@users.noreply.github.com>
2022-01-19 11:59:04 +0800

    Update app.py
    新增了腾讯云环境变量读取用户cookie

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 # -*- coding: UTF-8 -*-
 #!/usr/bin/env python3
-import os
 from flask import Flask, jsonify, render_template, request, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import control as driver
+import os
 IS_SERVERLESS = bool(os.environ.get('SERVERLESS'))
+cookie = os.environ.get('cookie')
 app = Flask(__name__)
 
 config = {
     #为了方便二次开发，此处可添加多个用户信息，格式[{'name':'名称1','cookie':'京豆cookie'}, {'name':'名称2','cookie':'京豆cookie'}]
     'userInfo': [
         {
-            "name": "这里填写账号",#账号标识名称，可任意填写
-            "cookie": '''这里填写cookie'''
+            "name": "FSHOW"",#账号标识名称，可任意填写
+            "cookie": cookie
         }
     ],
 
@@ -40,11 +41,12 @@ def index():
       kwgt_activateDay += createText1
       kwgt_beanNum += createText2
       num += 1
-  kwgt = f'''
-  ++headImageUrl++{data['headImageUrl']}--headImageUrl--++isJTH++{data['isJTH']}--isJTH--++isPlusVip++{data['isPlusVip']}--isPlusVip--++jdNum++{data['jdNum']}--jdNum--++jvalue++{data['jvalue']}--jvalue--++levelName++{data['levelName']}--levelName--++nickname++{data['nickname']}--nickname--++curPin++{data['curPin']}--curPin--{kwgt_activateDay}{kwgt_beanNum}'''
+  kwgt = f'''{kwgt_activateDay}{kwgt_beanNum}'''
 
   return kwgt
 
-
+@app.route("/cookie")
+def getCookie():
+    return str(cookie)
 # 启动服务，监听 9000 端口，监听地址为 0.0.0.0
 app.run(debug=IS_SERVERLESS != True, port=9000, host='0.0.0.0')
