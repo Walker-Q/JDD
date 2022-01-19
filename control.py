92ea8459b8a74b40d9f78d7bff824630c1086658
F兽 <54022108+aqz236@users.noreply.github.com>
2022-01-19 11:59:32 +0800

    Update control.py
    弃用了getUserInfo部分

--- control.py
+++ control.py
@@ -5,7 +5,8 @@ import requests
 def control(config):
     for i in config['userInfo']:
         changesData = getChanges(i)
-        userIndoDic = getUserInfo(i['cookie'])
+        # userIndoDic = getUserInfo(i['cookie'])# 为了缩减网络请求所耗的时间，这部分放到了前端
+        userIndoDic = {}
         userIndoDic['changesData'] = changesData
         print(userIndoDic)
         print("\n\n")
@@ -47,9 +48,9 @@ def getChanges(info):
     return dataList
 
 
-
+#下面信息目前弃用，仅作参考
 def getUserInfo(cookie):
-    import requests, re
+    import requests, re,time
     url = 'https://wq.jd.com/user/info/QueryJDUserInfo?sceneid=80027&_=1642224937161&sceneval=2&g_login_type=1&callback=getUserInfoCb&g_ty=ls'
     headers = {'Host': 'wq.jd.com', 'Connection': 'keep-alive',
                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36',
@@ -57,17 +58,32 @@ def getUserInfo(cookie):
                'Referer': 'https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2&jxsid=16422247425862224986&ptag=7155.1.17',
                'Accept-Encoding': 'gzip, deflate, br', 'Accept-Language': 'zh-CN,zh;q=0.9'}
     cookies = {i.split("=")[0]: i.split("=")[1] for i in cookie.split("; ")}
-    html = requests.get(url, headers=headers, cookies=cookies)
-    htmll = html.text
-    userIndoDic = {}
-    userIndoDic["headImageUrl"] = re.findall(r'"headImageUrl" : "(.*?)",', htmll, re.I)[0]
-    userIndoDic["curPin"] = re.findall(r'"curPin" : "(.*?)",', htmll, re.I)[0]
-    userIndoDic["isJTH"] = re.findall(r'"isJTH" : "(.*?)",', htmll, re.I)[0]
-    userIndoDic["jdNum"] = re.findall(r'"jdNum" : (.*?),', htmll, re.I)[0]
-    userIndoDic["jvalue"] = re.findall(r'"jvalue" : (.*?),', htmll, re.I)[0]
-    userIndoDic["levelName"] = re.findall(r'"levelName" : "(.*?)",', htmll, re.I)[0]
-    userIndoDic["nickname"] = re.findall(r'"nickname" : "(.*?)",', htmll, re.I)[0]
-    userIndoDic["isPlusVip"] = re.findall(r'"isPlusVip" : (.*?),', htmll, re.I)[0]
+    
+    try:
+        html = requests.get(url, headers=headers, cookies=cookies)
+        htmll = html.text
+        userIndoDic = {}
+        userIndoDic["headImageUrl"] = re.findall(r'"headImageUrl" : "(.*?)",', htmll, re.I)[0]
+        userIndoDic["curPin"] = re.findall(r'"curPin" : "(.*?)",', htmll, re.I)[0]
+        userIndoDic["isJTH"] = re.findall(r'"isJTH" : "(.*?)",', htmll, re.I)[0]
+        userIndoDic["jdNum"] = re.findall(r'"jdNum" : (.*?),', htmll, re.I)[0]
+        userIndoDic["jvalue"] = re.findall(r'"jvalue" : (.*?),', htmll, re.I)[0]
+        userIndoDic["levelName"] = re.findall(r'"levelName" : "(.*?)",', htmll, re.I)[0]
+        userIndoDic["nickname"] = re.findall(r'"nickname" : "(.*?)",', htmll, re.I)[0]
+        userIndoDic["isPlusVip"] = re.findall(r'"isPlusVip" : (.*?),', htmll, re.I)[0]
+    except:#错误就重试一次
+        html = requests.get(url, headers=headers, cookies=cookies)
+        htmll = html.text
+        userIndoDic = {}
+        userIndoDic["headImageUrl"] = re.findall(r'"headImageUrl" : "(.*?)",', htmll, re.I)[0]
+        userIndoDic["curPin"] = re.findall(r'"curPin" : "(.*?)",', htmll, re.I)[0]
+        userIndoDic["isJTH"] = re.findall(r'"isJTH" : "(.*?)",', htmll, re.I)[0]
+        userIndoDic["jdNum"] = re.findall(r'"jdNum" : (.*?),', htmll, re.I)[0]
+        userIndoDic["jvalue"] = re.findall(r'"jvalue" : (.*?),', htmll, re.I)[0]
+        userIndoDic["levelName"] = re.findall(r'"levelName" : "(.*?)",', htmll, re.I)[0]
+        userIndoDic["nickname"] = re.findall(r'"nickname" : "(.*?)",', htmll, re.I)[0]
+        userIndoDic["isPlusVip"] = re.findall(r'"isPlusVip" : (.*?),', htmll, re.I)[0]
+    
     return userIndoDic
 
 if __name__ == '__main__':
